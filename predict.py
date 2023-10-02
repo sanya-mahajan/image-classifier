6fc73def64f143177aa954feb6e84a4c06ebea7d
Sanya <88281057+sanya-mahajan@users.noreply.github.com>
2023-10-02 22:36:43 +0530

    add

--- predict.py
+++ predict.py
@@ -26,5 +26,59 @@ def process_image(image_path):
     image = transform(image)
     return image
 
-# Function to predict the class of an image
-def predict(image_path, model, topk, category_names, device
+def predict(image_path, model, topk, category_names, device):
+    # Process the image
+    image = process_image(image_path)
+    image = image.unsqueeze(0).to(device)
+
+    # Set the model to evaluation mode
+    model.eval()
+    
+    # Make the prediction
+    with torch.no_grad():
+        output = model(image)
+
+    # Calculate probabilities and top K classes
+    probabilities = torch.exp(output)
+    top_p, top_class = probabilities.topk(topk, dim=1)
+
+    # Convert indices to classes
+    idx_to_class = {v: k for k, v in model.class_to_idx.items()}
+    top_classes = [idx_to_class[i] for i in top_class[0].tolist()]
+
+    # If category_names are provided, convert class indices to actual names
+    if category_names:
+        with open(category_names, 'r') as f:
+            cat_to_name = json.load(f)
+        top_class_names = [cat_to_name[c] for c in top_classes]
+    else:
+        top_class_names = top_classes
+
+    return top_p[0].tolist(), top_class_names
+
+def main():
+    parser = argparse.ArgumentParser(description="Image Classifier Prediction")
+    parser.add_argument("image_path", help="Path to the image for prediction")
+    parser.add_argument("checkpoint", help="Path to the trained model checkpoint")
+    parser.add_argument("--top_k", type=int, default=5, help="Return top K most likely classes")
+    parser.add_argument("--category_names", help="Path to a JSON file that maps category names")
+    parser.add_argument("--gpu", action="store_true", help="Use GPU for inference")
+
+    args = parser.parse_args()
+
+    # Determine the device (GPU or CPU)
+    device = "cuda" if args.gpu and torch.cuda.is_available() else "cpu"
+
+    # Load the model from the checkpoint
+    model = load_checkpoint(args.checkpoint)
+    model.to(device)
+
+    # Perform prediction
+    top_p, top_class_names = predict(args.image_path, model, args.top_k, args.category_names, device)
+
+    # Print the results
+    for i in range(args.top_k):
+        print(f"Prediction {i+1}: Class='{top_class_names[i]}' | Probability={top_p[i]*100:.2f}%")
+
+if __name__ == "__main__":
+    main()
